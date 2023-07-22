58f6d8c2b58d5edb974ff53e0d2859c39d7de27d
mukhammadjondev <muhsdev@gmail.com>
2023-07-22 18:04:26 +0500

    Add modal and menu section

--- js/script.js
+++ js/script.js
@@ -99,4 +99,103 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline)
+
+    // Modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal')
+
+    function closeModal() {
+        modal.classList.add('hide')
+        modal.classList.remove('show')
+        document.body.style.overflow = ''
+    }
+
+    function openModal() {
+        modal.classList.add('show')
+        modal.classList.remove('hide')
+        document.body.style.overflow = 'hidden'
+        clearInterval(modalTimer)
+    }
+    modalTrigger.forEach(i => i.addEventListener('click', openModal))
+
+    modal.addEventListener('click', (e)=> {
+        if(e.target == modal || e.target.getAttribute('data-close') == '') {
+            closeModal()
+        }
+    })
+
+    document.addEventListener('keydown', (e)=> {
+        if(e.code == 'Escape' && modal.classList.contains('show')) {
+            closeModal()
+        }
+    })
+
+    const modalTimer = setTimeout(openModal, 500000)
+
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal()
+            window.removeEventListener('scroll', showModalByScroll)
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll)
+
+    // Class
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src
+            this.alt = alt
+            this.title = title
+            this.descr = descr
+            this.price = price
+            this.classes = classes
+            this.parent = document.querySelector(parentSelector)
+            this.transfer = 11483
+            this.changeToUZS()
+        }
+
+        changeToUZS() {
+            Math.floor(this.price = this.price * this.transfer)
+        }
+
+        render() {
+            const element = document.createElement('div')
+            if(this.classes.length === 0) {
+                this.element = 'menu__item'
+                element.classList.add(this.element)
+            }else{
+                this.classes.forEach(classname => element.classList.add(classname))
+            }
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt} />
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Price:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> uzs/month</div>
+                </div>
+            `
+
+            this.parent.append(element)
+        }
+    }
+
+    axios.get('http://localhost:3000/menu').then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+            })
+        })
+
+    // async function getRecource(url) {
+    //     const res = await fetch(url)
+
+    //     return await res.json()
+    // }
+
+    // getRecource('http://localhost:3000/menu').then(data => {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+    //     })
+    // })
 })
